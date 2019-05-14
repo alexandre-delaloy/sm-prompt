57da2ce0e8a2a27d77984f61520503056e475762
blyndusk <alexandredelaloydev@gmail.com>
2019-05-14 18:45:36 +0200

    :sparkles: add exit code, root user & 2-lines interface

--- sm-theme.zsh
+++ sm-theme.zsh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-PROMPT='%F{blue}%n%F{yellow}@%F{cyan}%M %F{yellow}→ %F{default}$(git_prompt_info) $(git_prompt_status)'
-RPROMPT='%? - %F{magenta}%D%F{yellow}/%F{magenta}%T%F{default}'
+local isroot="blue";
 
+if [ "$UID" -eq 0 ] ; then isroot="red" ; fi
+
+local return_code="%(?..%F{red}%? - )%F{default}"
+
+PROMPT='%F{yellow}╭─%F{${isroot}}%n%F{yellow}@%F{cyan}%M $(git_prompt_info) $(git_prompt_status)%F{default} ${dude}
+%F{yellow}╰───→ %F{default}'
+RPROMPT='${return_code}%F{magenta}%D%F{yellow} /%F{magenta}%t%F{default}'
 ZSH_THEME_GIT_PROMPT_PREFIX="%F{magenta}git:%F{default}"
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
 ZSH_THEME_GIT_PROMPT_DIRTY=" %F{red}✘%F{default}"
@@ -13,4 +19,4 @@ ZSH_THEME_GIT_PROMPT_MODIFIED="%F{yellow}◎%F{default} "
 ZSH_THEME_GIT_PROMPT_DELETED="%F{red}○%F{default} "
 ZSH_THEME_GIT_PROMPT_RENAMED="%F{blue}‣%F{default} "
 ZSH_THEME_GIT_PROMPT_UNMERGED="%F{magenta}✖%F{default} "
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{magenta}‽%F{default} "
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{magenta}✦%F{default} "
