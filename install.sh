f66df6c4ac89f568eac6482a5e02a15c1b32febc
blyndusk <alexandredelaloydev@gmail.com>
2019-10-01 14:10:52 +0200

    🐛 fix install bash script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ install_zsh() {
     if [ -d !~/.oh-my-zsh ] ; then echo -e "$sad_face It seems that there is no \e[1;34moh-my-zsh\e[0m folder at the root of this user"
     else 
         cp ./sm.zsh-theme ~/.oh-my-zsh/themes
-        if [ -f ~/.oh-my-zsh/themes/sm.zsh-theme ]; then echo -e "$happy_face Congrats! New\e[1;34m zsh theme (sm-theme)\e[0m added !" ; 
+        if [ -f ~/.oh-my-zsh/themes/sm.zsh-theme ]; then echo -e "$happy_face Congrats! \e[1;34msm-theme\e[0m is installed for \e[1;34mzsh\e[0m !" ; 
         else echo -e "$sad_face Uh oh.. Something went wrong.." ; fi
     fi
 }
@@ -17,7 +17,7 @@ install_zsh() {
 install_bash() {
     cp ./sm.bash-theme ~
     if ! grep "source ./sm.bash-theme" ~/.bashrc | clear; then printf "\n\nsource ./sm.bash-theme" >> ~/.bashrc ; fi
-    if [ -f ~/sm.bash-theme ]; then echo -e "$happy_face Congrats! New\e[1;34m bash theme (sm-theme)\e[0m added !"
+    if [ -f ~/sm.bash-theme ]; then echo -e "$happy_face Congrats! \e[1;34msm-theme\e[0m is installed for \e[1;34mbash\e[0m !" ;
     else echo -e "$sad_face Uh oh.. Something went wrong.." ; fi
 }
 
@@ -27,4 +27,4 @@ elif [[ "$1" = "--all" || "$1" = "-a" ]]  ; then
     install_zsh
     install_bash
     clear ; echo -e "$happy_face Congrats! All\e[1;34m sm-theme\e[0m added !"
-fi
+else echo "$sad_face Please specify an environment, ie '\e[1;34m--zsh\e[0m' or '\e[1;34m--bash\e[0m'" ; fi
