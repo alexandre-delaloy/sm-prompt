e1211b748e4498925eff74327a3b89294af895bf
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-04-09 00:13:54 +0200

    :ok_hand: finish light install script

--- install.light.sh
+++ install.light.sh
@@ -1,6 +1,11 @@
-install_zsh() {
+
+R="\e[0;31m[?]\e[0m";
+G="\e[0;32m[v]\e[0m";
+Y="\e[0;33m[?]\e[0m";
+
+install_theme() {
   local CURRENT_FILE=sm.v2.zsh-theme;
-  local STATUS="";
+  STATUS="";
   if [ ! -f ~/.sm.zsh-theme ] ; then
     # the file doesn't and will installed
     STATUS="installed"
@@ -10,22 +15,34 @@ install_zsh() {
   fi
   # copy the desired file to the file destination
   cp $CURRENT_FILE ~/.sm.zsh-theme;
+}
 
-  if [ -f ~/.zshrc ] ; then
-    source ~/.zshrc;
-    source ~/.sm.zsh-theme;
-    clear;
-    echo "\e[0;32msm-prompt\e[0m correctly $STATUS.";
-  else 
-    echo "\e[0;33m.zshrc\e[0m file was \e[0;31mnot\e[0m found."
-
+check_config_file_existence() {
+  if [ ! -f ~/.zshrc ] ; then
+    echo "$Y .zshrc file was not found."
+    touch ~/.zshrc;
+    echo "$G .zshrc file has been created."
+  else
+    echo "$G .zshrc file exist."
   fi
+}
 
+source_theme() {
   local STRING="source ~/.sm.zsh-theme"
   if [ $(cat ~/.zshrc | grep $STRING | wc -l) -eq 0 ] ; then
-    echo "Theme \e[0;32msourced\e[0m."
     echo "\n$STRING" >> ~/.zshrc;
+    source ~/.zshrc;
+    echo "$G Theme sourced.";
   fi
+  source ~/.sm.zsh-theme;
+}
+
+install_zsh() {
+  install_theme
+  check_config_file_existence
+  source_theme
+  
+  echo "$G sm-prompt correctly $STATUS.";
 }
 
 if [ "$1" = "--zsh" ]; then
