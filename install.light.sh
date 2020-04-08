768bb629669287626681c0f61f6d869527617bd4
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-04-09 01:21:44 +0200

    :bug: fix prompts bugs

--- install.light.sh
+++ install.light.sh
@@ -3,10 +3,14 @@ R="\e[0;31m[?]\e[0m";
 G="\e[0;32m[v]\e[0m";
 Y="\e[0;33m[?]\e[0m";
 
-install_theme() {
-  local CURRENT_FILE=sm.v2.zsh-theme;
+CONFIG_FILE=.zshrc;
+THEME_FILE=sm.zsh-theme;
+
+clear;
+
+copy_theme() {
   STATUS="";
-  if [ ! -f ~/.sm.zsh-theme ] ; then
+  if [ ! -f ~/.$THEME_FILE ] ; then
     # the file doesn't and will installed
     STATUS="installed"
   else
@@ -14,37 +18,45 @@ install_theme() {
     STATUS="updated"
   fi
   # copy the desired file to the file destination
-  cp $CURRENT_FILE ~/.sm.zsh-theme;
+  cp $THEME_FILE ~/.$THEME_FILE;
 }
 
-check_config_file_existence() {
-  if [ ! -f ~/.zshrc ] ; then
-    echo "$Y .zshrc file was not found."
-    touch ~/.zshrc;
-    echo "$G .zshrc file has been created."
+check_rc_file_existence() {
+  if [ ! -f ~/$CONFIG_FILE ] ; then
+    echo "$Y $CONFIG_FILE file was not found.";
+    touch ~/$CONFIG_FILE;
+    echo "$G $CONFIG_FILE file has been created.";
   else
-    echo "$G .zshrc file exist."
+    echo "$G $CONFIG_FILE file exist.";
   fi
 }
 
 source_theme() {
-  local STRING="source ~/.sm.zsh-theme"
-  if [ $(cat ~/.zshrc | grep $STRING | wc -l) -eq 0 ] ; then
-    echo "\n$STRING" >> ~/.zshrc;
-    source ~/.zshrc;
+  local STRING="source ~/.$THEME_FILE"
+  if [ $(cat ~/$CONFIG_FILE | grep $STRING | wc -l) -eq 0 ] ; then
+    echo "\n$STRING" >> ~/$CONFIG_FILE;
+    source ~/$CONFIG_FILE;
     echo "$G Theme sourced.";
   fi
-  source ~/.sm.zsh-theme;
+  source ~/.$THEME_FILE;
 }
 
-install_zsh() {
-  install_theme
-  check_config_file_existence
-  source_theme
-  
-  echo "$G sm-prompt correctly $STATUS.";
+install_theme() {
+  copy_theme;
+  check_rc_file_existence;
+  source_theme;
+  local SHELL=${CONFIG_FILE//[.|rc]/""};
+  echo "$G sm-prompt correctly $STATUS for $SHELL.";
 }
 
 if [ "$1" = "--zsh" ]; then
-  install_zsh
+  CONFIG_FILE="$CONFIG_FILE";
+  THEME_FILE="$THEME_FILE";
+  echo "$Y You've choose zsh shell"
+elif [ "$1" = "--bash" ]; then
+  CONFIG_FILE=".bashrc";
+  THEME_FILE="sm.bash-theme";
+  echo "$Y You've choose bash shell"
 fi
+
+install_theme ;
