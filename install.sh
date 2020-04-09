796f5eceebf7e03695ea937aa1becdf7c168dfc8
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-04-09 17:54:37 +0200

    :boom: while loop according to shell

--- install.sh
+++ install.sh
@@ -4,22 +4,19 @@ R="\e[0;31m[!]\e[0m";
 G="\e[0;32m[v]\e[0m";
 Y="\e[0;33m[?]\e[0m";
 
-clear;
-
-FLAG="$1"
-RC_FILE=.bashrc;
-THEME_LOCATION=~/
-ORIGINAL_FILE=sm.bash-theme
-THEME_FILE=.sm.bash-theme;
+FLAG="$1";
+RC_FILE=.zshrc;
+THEME_LOCATION=~/;
+ORIGINAL_FILE=sm.zsh-theme;
+THEME_FILE=.sm.zsh-theme;
 STATUS="installed";
 
 copy_theme() {
   if [ -f $THEME_LOCATION$THEME_FILE ]; then
-    STATUS="updated"
+    STATUS="updated";
   fi
-
   if [[ $1 = "OMZ" && ! -d $THEME_LOCATION ]] ; then 
-    echo "$R $THEME_LOCATION folder not found"
+    echo "$R $THEME_LOCATION folder not found";
   fi
   
   cp $ORIGINAL_FILE $THEME_LOCATION$THEME_FILE;
@@ -45,7 +42,7 @@ check_rc_file_existence() {
 
 source_theme() {
   SOURCE="if [ -f $THEME_LOCATION$THEME_FILE ] ; then source $THEME_LOCATION$THEME_FILE ; fi";
-  source $THEME_LOCATION$THEME_FILE
+  source $THEME_LOCATION$THEME_FILE;
   if [ $1 != "OMZ" ]; then
     if [ $(cat ~/$RC_FILE | grep "source $THEME_LOCATION$THEME_FILE" | wc -l) -eq 0 ] ; then
       echo "$SOURCE" >> ~/$RC_FILE;
@@ -61,7 +58,7 @@ install_theme() {
   copy_theme $1;
   check_rc_file_existence $1;
   source_theme $1;
-  IS_INSTALLED=false
+  IS_INSTALLED=false;
   if [ -f $THEME_LOCATION$THEME_FILE ]; then 
     if [ $1 = "OMZ" ] ; then
       if [ $(cat ~/$RC_FILE | grep $SOURCE | wc -l) -ne 0 ]; then 
@@ -83,16 +80,12 @@ install_theme() {
 confirm() {
   printf "$Y You've choose $1 shell. Continue ? [Y/n] ";
   read -r REPLY;
-  
   if [[ $REPLY =~ ^([yY][eE][sS]|[yY])$ ]] ; then
-    
-    if [ $FLAG = "--zsh" ]; then
+    if [ $1 = "ZSH" ]; then
       printf "$Y Using OH-MY-ZSH ? [Y/n] ";
       read -r REPLY;
       if [[ $REPLY =~ ^([yY][eE][sS]|[yY])$ ]] ; then
         echo "$G Using Oh-MY-ZSH"
-        RC_FILE=.zshrc;
-        ORIGINAL_FILE=sm.zsh-theme;
         THEME_FILE=sm.zsh-theme;
         THEME_LOCATION=~/.oh-my-zsh/themes/
         install_theme "OMZ"
@@ -101,13 +94,13 @@ confirm() {
         fi
       else 
         echo "$G Using ZSH"
-        RC_FILE=.zshrc;
-        ORIGINAL_FILE=sm.zsh-theme;
-        THEME_FILE=.sm.zsh-theme;
         install_theme "ZSH";
       fi
-    elif [ $FLAG = "--bash" ]; then
+    elif [ $1 = "BASH" ]; then
       echo "$G Using BASH"
+      RC_FILE=.bashrc;
+      ORIGINAL_FILE=sm.bash-theme;
+      THEME_FILE=.sm.bash-theme;
       install_theme "BASH";
     fi
   else
@@ -115,8 +108,22 @@ confirm() {
   fi
 }
 
-if [ $FLAG = "--zsh" ]; then
-  confirm "ZSH"
-elif [ $FLAG = "--bash" ]; then
+select_shell() {
+  printf "$Y Select your current Shell [bash/zsh]: "
+  read -r REPLY;
+  FLAG=$REPLY
+}
+
+clear;
+select_shell
+while ! [[ $REPLY =~ ^([bB][aB][sS][hH]|[zZ][sS][hH])$ ]] 
+do
+  echo "$R Shell name invalid"
+  select_shell
+done
+
+if [[ $FLAG =~ ^([bB][aB][sS][hH])$ ]]; then
   confirm "BASH"
+elif [[ $FLAG =~ ^([zZ][sS][hH])$ ]]; then
+  confirm "ZSH"
 fi
